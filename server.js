8e9b726b640ed10c3d54201f77db18397b4029c3
Devasri S <devasrisundar123@gmail.com>
2026-06-16 15:21:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Devsundar06#",
+    password: "   ",
     database: "taskmanager"
 });
 
